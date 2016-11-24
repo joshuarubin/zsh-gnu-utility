6453bd31c8a1ba421f7ff549cdb84a0ad4050c6c
Joshua Rubin <jrubin@zvelo.com>
2016-11-23 22:52:04 -0700

    fix conflict with zsh-autosuggest

--- gnu-utility.plugin.zsh
+++ gnu-utility.plugin.zsh
@@ -5,14 +5,17 @@
 #   Sorin Ionescu <sorin.ionescu@gmail.com>
 #
 
+# Get the prefix or use the default.
+zstyle -s ':prezto:module:gnu-utility' prefix '_gnu_utility_p' || _gnu_utility_p='g'
+
 # Return if requirements are not found.
-if (( ! ${+commands[gwhoami]} )); then
+if (( ! ${+commands[${_gnu_utility_p}whoami]} )); then
   return 1
 fi
 
 _gnu_utility_cmds=(
   # Coreutils
-  '[' 'base64' 'basename' 'cat' 'chcon' 'chgrp' 'chmod' 'chown'
+  'base64' 'basename' 'cat' 'chcon' 'chgrp' 'chmod' 'chown'
   'chroot' 'cksum' 'comm' 'cp' 'csplit' 'cut' 'date' 'dd' 'df'
   'dir' 'dircolors' 'dirname' 'du' 'env' 'expand' 'expr'
   'factor' 'false' 'fmt' 'fold' 'groups' 'head' 'hostid' 'id'
@@ -44,7 +47,7 @@ _gnu_utility_cmds=(
 
 # Wrap GNU utilities in functions.
 for _gnu_utility_cmd in "${_gnu_utility_cmds[@]}"; do
-  _gnu_utility_pcmd="g${_gnu_utility_cmd}"
+  _gnu_utility_pcmd="${_gnu_utility_p}${_gnu_utility_cmd}"
   if (( ${+commands[${_gnu_utility_pcmd}]} )); then
     eval "
       function ${_gnu_utility_cmd} {
